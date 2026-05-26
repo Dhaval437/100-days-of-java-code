7843adb32b41d423a42d573ef2e0715dfd1ca37c
Dhaval <dhavalgandhi98221@gmail.com>
2026-05-26 15:09:22 +0530

    Add files via upload

---
+++ day47.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class day47 {
+    public static void main(String[]args){
+        int arr[]={5,4,1,3,2};
+        Arrays.sort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }   
+}
+
