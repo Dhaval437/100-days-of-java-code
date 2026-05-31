04a67c4bb49feef6e0db0e082d2c8e905c4d8a96
Dhaval <dhavalgandhi98221@gmail.com>
2026-05-31 13:43:55 +0530

    Add files via upload

---
+++ day50.java
@@ -0,0 +1,24 @@
+public class day50 {
+    public static void bubble_sort(int arr[]){
+        for(int turns=0;turns<arr.length-1;turns++){
+            for(int j=0;j<arr.length-1-turns;j++){
+                if(arr[j]<arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={3,6,2,1,8,7,4,5,3,1};
+        bubble_sort(arr);
+        printArr(arr);
+    }
+} 
+
