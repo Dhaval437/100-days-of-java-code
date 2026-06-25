f8f3a7b94f7663ffb391c372e4580094c0b081c2
Dhaval <dhavalgandhi98221@gmail.com>
2026-06-25 10:09:54 +0530

    Add files via upload

---
+++ day70.java
@@ -0,0 +1,12 @@
+public class day70{
+    public static void print_array(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        print_array(arr);
+    }
+}
+
