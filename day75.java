a1bba4091bb09588bf14f951e11c7e63a977a833
Dhaval <dhavalgandhi98221@gmail.com>
2026-07-03 22:35:07 +0530

    Add files via upload

---
+++ day75.java
@@ -0,0 +1,13 @@
+public class day75 {
+    public static void main(String[] args) {
+        String s1="tony";
+        String s2="tony";
+        if(s1.equals(s2)){
+            System.out.println("string is equal");
+        }
+        else{
+            System.out.println("string is not equal");
+        }
+    }
+}
+
