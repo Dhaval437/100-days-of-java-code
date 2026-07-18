1514b3dd86b88fffc33b50c73df88c3bb42e699a
Dhaval <dhavalgandhi98221@gmail.com>
2026-07-18 22:42:52 +0530

    Add files via upload

---
+++ day88.java
@@ -0,0 +1,15 @@
+public class day88{
+    public static void print_pattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*"+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int n=4;
+        print_pattern(n);
+    }
+}
+
